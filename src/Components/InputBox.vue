73a0e4a03b4ba9d6e94d464c47ec09131dae81b9
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-03-06 12:35:34 +0000

    Added input groups to input box component

--- src/Components/InputBox.vue
+++ src/Components/InputBox.vue
@@ -2,8 +2,24 @@
     <div class="form-group" :class="{'has-error': showError }">
         <label class="control-label" :class="{'col-sm-2': !stacked}">{{ label }}: <span v-if="required">*</span></label>
         <div :class="{'col-sm-10': !stacked}">
-            <input :type="type" v-on:input="updateValue($event.target.value)" class="form-control"
-                   :name="name" :id="id" :readonly="readonly" :value="value" :placeholder="placeholder">
+            <div class="input-group">
+                <div class="input-group-addon" v-if="slotExists('leftAddon')">
+                    <slot name="leftAddon"></slot>
+                </div>
+                <div class="input-group-btn" v-if="slotExists('leftBtn')">
+                    <slot name="leftBtn"></slot>
+                </div>
+
+                <input :type="type" v-on:input="updateValue($event.target.value)" class="form-control"
+                       :name="name" :id="id" :readonly="readonly" :value="value" :placeholder="placeholder">
+
+                <div class="input-group-addon" v-if="slotExists('rightAddon')">
+                    <slot name="rightAddon"></slot>
+                </div>
+                <div class="input-group-btn" v-if="slotExists('rightBtn')">
+                    <slot name="rightBtn"></slot>
+                </div>
+            </div>
             <p class="text-danger" v-if="showError" v-text="errorMessage"></p>
         </div>
         <div class="clearfix"></div>
@@ -39,6 +55,10 @@
         methods: {
             updateValue(value) {
                 this.$emit('input', value)
+            },
+
+            slotExists(name) {
+                return (name in this.$slots);
             }
         }
     };
