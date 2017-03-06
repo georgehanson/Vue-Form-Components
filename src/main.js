f3ac32c9400b8e5848472d3644b6c29f4b26ef82
George Hanson <george.w.hanson96@gmail.com>
2017-03-06 10:46:37 +0000

    Fixed export in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import InputBoxComponent from './Components/InputBox.vue';
 import SwitchComponent from './Components/Switch.vue';
+import TextAreaComponent from './Components/TextArea.vue';
 
 exports.InputBoxComponent = InputBoxComponent;
 exports.SwitchComponent = SwitchComponent;
+exports.TextAreaComponent = TextAreaComponent;
