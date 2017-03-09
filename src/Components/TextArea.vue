8833e44faef06539b6b47a62666b49afa48e7f56
Simon Rogers <simonrogers@gmlconsulting.co.uk>
2017-03-09 14:36:07 +0000

    Add in helper text option for components

--- src/Components/TextArea.vue
+++ src/Components/TextArea.vue
@@ -5,6 +5,7 @@
             <textarea :name="name" id="name" rows="5" v-on:input="updateValue($event.target.value)"
                       :value="value" class="form-control"></textarea>
             <p class="text-danger" v-if="showError" v-text="errorMessage"></p>
+            <p class="help-block" v-text="helper" v-if="helper"></p>
         </div>
         <div class="clearfix"></div>
     </div>
@@ -20,6 +21,7 @@
                 type: String,
                 required: true
             },
+            helper: String,
             showError: Boolean,
             stacked: Boolean,
             value: [String],
