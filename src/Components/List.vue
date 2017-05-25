9d0288563fc00ca74399214c5970292ee97dc11f
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-05-25 10:30:53 +0100

    Add input group options to list item

--- src/Components/List.vue
+++ src/Components/List.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="form-group" :class="{'has-error': showError }">
-        <label class="control-label" :class="{'col-sm-2': !stacked}" v-if="label">{{ label }}: <span v-if="required">*</span></label>
+        <label class="control-label" :class="{'col-sm-2': !stacked}" v-if="label">{{ label }}: <span
+                v-if="required">*</span></label>
         <div :class="{'col-sm-10': (!stacked && label)}">
-            <select :name="name" :id="name" class="form-control" v-on:change="updateValue($event.target.value)">
-                <option value="" disabled readonly v-text="placeholder" v-if="placeholder" :selected="value == null || value == ''"></option>
-                <option v-for="option in options" :value="option[keyName]" v-text="option[labelName]" :selected="option[keyName] == value"></option>
-            </select>
+            <div :class="{'input-group': usingAddons}">
+                <div class="input-group-addon" v-if="slotExists('leftAddon')">
+                    <slot name="leftAddon"></slot>
+                </div>
+                <div class="input-group-btn" v-if="slotExists('leftBtn')">
+                    <slot name="leftBtn"></slot>
+                </div>
+                <select :name="name" :id="name" class="form-control" v-on:change="updateValue($event.target.value)">
+                    <option value="" disabled readonly v-text="placeholder" v-if="placeholder"
+                            :selected="value == null || value == ''"></option>
+                    <option v-for="option in options" :value="option[keyName]" v-text="option[labelName]"
+                            :selected="option[keyName] == value"></option>
+                </select>
+                <div class="input-group-addon" v-if="slotExists('rightAddon')">
+                    <slot name="rightAddon"></slot>
+                </div>
+                <div class="input-group-btn" v-if="slotExists('rightBtn')">
+                    <slot name="rightBtn"></slot>
+                </div>
+            </div>
 
             <p class="text-danger" v-if="showError" v-text="errorMessage"></p>
             <p class="help-block" v-text="helper" v-if="helper"></p>
@@ -49,15 +66,25 @@
         },
 
         methods: {
+            slotExists(name) {
+                return (name in this.$slots);
+            },
+            
             updateValue(value) {
                 this.$emit('input', value)
             }
         },
 
+        computed: {
+            usingAddons() {
+                return ! (Object.keys(this.$slots).length === 0 && this.$slots.constructor === Object)
+            }
+        },
+
         updated() {
-            if(this.options.length) {
+            if (this.options.length) {
                 let element = document.getElementById(this.name);
-                if(element.options[element.selectedIndex]) {
+                if (element.options[element.selectedIndex]) {
                     this.updateValue(element.options[element.selectedIndex].value);
                 }
             }
