f5f9acc4789c834c681eaf1094e7d4043d053a00
Matthew Daly <matthewdaly@gmlconsulting.co.uk>
2017-09-29 17:42:55 +0100

    Added test for blur event

--- tests/inputbox.test.js
+++ tests/inputbox.test.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import InputBoxComponent from '../src/Components/InputBox.vue';
+import { shallow } from 'avoriaz';
 
 describe('InputBoxComponent', () => {
     test("it generates the markup", () => {
@@ -22,5 +23,18 @@ describe('InputBoxComponent', () => {
         renderer.renderToString(vm, (err, str) => {
             expect(str).toMatchSnapshot();
         });
+    });
+
+    test("it fires the blur event", () => {
+        const wrapper = shallow(InputBoxComponent, {
+            props: {
+                name: 'name',
+                label: 'Name'
+            }
+        });
+        wrapper.vm.loseFocus = jest.fn();
+        let input = wrapper.first('input');
+        input.trigger('blur');
+        expect(wrapper.vm.loseFocus).toHaveBeenCalled();
     });
 });
