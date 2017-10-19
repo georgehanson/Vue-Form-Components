d648a811cd2428aded6151eb696f3ffb60879202
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-10-19 12:00:27 +0100

    Remove current system

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import InputBoxComponent from './Components/InputBox.vue';
-import SwitchComponent from './Components/Switch.vue';
-import TextAreaComponent from './Components/TextArea.vue';
-import ListComponent from './Components/List.vue';
-
-exports.InputBoxComponent = InputBoxComponent;
-exports.SwitchComponent = SwitchComponent;
-exports.TextAreaComponent = TextAreaComponent;
-exports.ListComponent = ListComponent;
