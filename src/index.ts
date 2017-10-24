23c2b157f1e8ef46f0dc50ae21637d454f93c9e8
George Hanson <georgehanson@gmlconsulting.co.uk>
2017-10-24 14:42:30 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import InputBox from "./input-box.ts";
 import SelectList from "./select-list.ts";
+import TextArea from "./text-area.ts";
 
 exports.InputBox = InputBox;
 exports.SelectList = SelectList;
+exports.InputBlock = TextArea;
