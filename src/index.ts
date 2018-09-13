6a07dfcf10d8548ec92cc95ac018c3a195999a13
George Hanson <george.hanson@htk.co.uk>
2018-09-13 11:19:35 +0100

    Add file browser to index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,10 @@ import InputBox from "./input-box.ts";
 import SelectList from "./select-list.ts";
 import TextArea from "./text-area.ts";
 import ToggleSwitch from "./toggle-switch.ts";
+import FileBrowser from './file-browser.ts';
 
 exports.InputBox = InputBox;
 exports.SelectList = SelectList;
 exports.InputBlock = TextArea;
 exports.ToggleSwitch = ToggleSwitch;
+exports.FileBrowser = FileBrowser;
